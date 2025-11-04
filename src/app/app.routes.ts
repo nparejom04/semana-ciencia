a9bb58886af10fed058b20ec0f8be6af464d0bc4
Eva F.M. <eafernandez@educarex.es>
2025-11-04 16:19:48 +0100

    v inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Juego } from './juego/juego';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'juego', component: Juego}
+];
